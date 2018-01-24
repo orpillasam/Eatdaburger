739bd2bb1ace5b0aa61599854c05a627d78847e4
orpillasam <31043722+orpillasam@users.noreply.github.com>
2018-01-23 22:15:34 -0800

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUE ('Brutal Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUE ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUE ('The Whopper', false);
